a1fbc2f191245d5209292405698737f68945de52
Marcus Girolneto <marcus.girolneto@gmail.com>
2019-08-20 22:19:15 -0300

    Adicionando migration de usuarios e primeiro teste

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,14 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ hello: 'rock' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Marcus Girolneto',
+    email: 'marcus.girolneto@gmail.com',
+    password_hash: '123456',
+  });
+  res.json(user);
+});
 export default routes;
