c7b3108a33673f5a0b21c6495d2a8fcc1a76d70b
Marcus Girolneto <marcus.girolneto@gmail.com>
2019-08-21 20:15:55 -0300

    Adicionando middleware de autenticacao

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -14,5 +14,10 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+    return res.json({ ok: true });
+  }
 }
 export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '66e36defd5da82ac7c4af669083d4c43',
+  expiresIn: '7d',
+};
